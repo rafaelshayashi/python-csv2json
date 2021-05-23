c4d3855fa917e1d27adeff2eae193c7379ed1a76
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-05-23 14:29:13 -0300

    :wrench: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-csv2json',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/rafaelshayashi/python-csv2json.git',
+    license='MIT',
+    author='Rafael Simionato Hayashi',
+    author_email='rafaelshayashi@gmail.com',
+    description='simple cli for testing porpuse'
+)
