0599ac333ba098b6f015bda556a3a392b4cc414d
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-05-23 21:56:31 -0300

    :sparkles: creating a entrypoint

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='python-csv2json',
     version='0.1.0',
+    scripts=['./bin/python-csv2json'],
     packages=[''],
     url='https://github.com/rafaelshayashi/python-csv2json.git',
     license='MIT',
