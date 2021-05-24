350e6742a202e4a557d647fed8b217616e90b556
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-05-23 22:42:43 -0300

    :bug: fix module locations

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='python-csv2json',
+    name='csv2json',
     version='0.1.0',
-    scripts=['./bin/python-csv2json'],
-    packages=[''],
+    scripts=['./bin/csv2json'],
+    packages=find_packages(),
+    include_package_data=True,
     url='https://github.com/rafaelshayashi/python-csv2json.git',
     license='MIT',
     author='Rafael Simionato Hayashi',
